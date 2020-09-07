4a9a7dbb199c954f1defab86b707f09ab43f58df
ynigam98 <61027107+ynigam98@users.noreply.github.com>
2020-09-07 18:01:26 -0400

    Multiple_Components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyInfo from "../src/components/info"
+import App from "./components/App"
 
-
-ReactDOM.render(<MyInfo />, document.getElementById("root") )
+ReactDOM.render(<App />, document.getElementById("root") )
