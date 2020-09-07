e619ec10fab3806cb8a137d263ecddbb773fbaf2
ynigam98 <61027107+ynigam98@users.noreply.github.com>
2020-09-07 17:47:14 -0400

    func_component

---
+++ src/components/info.js
@@ -0,0 +1,16 @@
+import React from "react"
+function MyInfo() {
+    return (
+      <div>
+        <h1> Yash Nigam</h1>
+        <p>This is a paragraph..</p>
+        <ul>
+          <li>Yes</li>
+          <li>No</li>
+          <li>Maybe</li>
+        </ul>
+      </div>
+    )
+  }
+
+  export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import MyInfo from "../src/components/info"
 
-function MyInfo() {
-  return (
-    <div>
-      <h1> Yash Nigam</h1>
-      <p>This is a paragraph..</p>
-      <ul>
-        <li>Yes</li>
-        <li>No</li>
-        <li>Maybe</li>
-      </ul>
-    </div>
-  )
-}
 
 ReactDOM.render(<MyInfo />, document.getElementById("root") )
